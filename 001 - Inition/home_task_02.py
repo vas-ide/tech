d6bd92ea7786bdfbee197b6e1def62d99275e061
vas-ide <vas.ide2022@gmail.com>
2022-08-21 18:50:04 +0300

    Cheh 01 Init HT-02

---
+++ 001 - Inition/home_task_02.py
@@ -0,0 +1,30 @@
+
+
+
+
+# _____01_____
+a = float(input('Rectangle side. - '))
+b = float(input('Rectangle side. - '))
+print('Square rectangle', a * b)
+# _____02_____
+a = float(input('First number. - '))
+b = float(input('Second number. - '))
+opr = input('Simple operation. -  ')
+if opr == '+':
+    print('Operation "+"', a + b)
+elif opr == '-':
+    print('Operation "-"', a - b)
+elif opr == '*':
+    print('Operation "*"', a * b)
+elif opr == '/':
+    print('Operation "-"', a / b)
+
+# _____03_____
+
+# _____04_____
+
+
+
+
+
+
