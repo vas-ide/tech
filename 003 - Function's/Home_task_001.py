2c21ef6a39e867f142746ae448e0e4c5f649917c
vas-ide <vas.ide2022@gmail.com>
2022-09-01 16:39:06 +0300

    Cheh 03
         In progress ___

--- 003 - Function's/Home_task_001.py
+++ 003 - Function's/Home_task_001.py
@@ -24,7 +24,35 @@ calc(1, 10)
 calc(-1, -10)
 calc(1, -10)
 # __________03__________
+def max_num(num1, num2, num3):
+    work_list = []
+    work_list.append(num1)
+    work_list.append(num2)
+    work_list.append(num3)
+    max1 = max(work_list)
+    work_list.remove(max1)
+    max2 = max(work_list)
+    print(f'First max number = {max1}, Second max number = {max2}')
+max_num(10, 15, 20)
 # __________04__________
+def human_flag(lst, flag=True):
+    new_lst = []
+    if flag == True:
+        for i in lst:
+            if i % 2 != 0:
+                new_lst.append(i)
+        print(f'True {new_lst}')
+        return new_lst
+
+    if flag == False:
+        for i in lst:
+            if i % 2 == 0:
+                new_lst.append(i)
+        print(f'False {new_lst}')
+        return False
+list_for_example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+human_flag(list_for_example, True)
+human_flag(list_for_example, False)
 # __________05__________
 # __________06__________
 # __________07_________
