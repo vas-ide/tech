558fe309973d6ae2447fb1c13dd8b7b34386b738
vas-ide-Manjaro <vas.ide2022@gmail.com>
2023-10-11 14:37:59 +0300

    tech

--- 007 Practise/course7/decorators/decorators.py
+++ 007 Practise/course7/decorators/decorators.py
@@ -34,20 +34,20 @@ if __name__ == '__main__':
 
 
 
-@<MY_DECOR_NAME>
-def sum1(a, b):
-    return a+b
-
-v = sum1(1, 2)
-
-@<MY_DECOR_NAME>
-def sum2(*args):
-    return sum(args)
-
-v1 = sum2(*list(range(0,1000)))
-
-@<MY_DECOR_NAME>
-def dv2(**kwargs):
-    return kwargs.values()
-
-v2 = dv2(**{'a': 1, 'b': 2})
+# @<MY_DECOR_NAME>
+# def sum1(a, b):
+#     return a+b
+#
+# v = sum1(1, 2)
+#
+# @<MY_DECOR_NAME>
+# def sum2(*args):
+#     return sum(args)
+#
+# v1 = sum2(*list(range(0,1000)))
+#
+# @<MY_DECOR_NAME>
+# def dv2(**kwargs):
+#     return kwargs.values()
+#
+# v2 = dv2(**{'a': 1, 'b': 2})
