1052d6d80219da485ef64b78cacb3ee1c29dac18
vas-ide-Manjaro <vas.ide2022@gmail.com>
2023-10-17 15:19:06 +0300

    requests & beautifulsoup

--- beautifulsoup/req_beautiful_start.py
+++ beautifulsoup/req_beautiful_start.py
@@ -14,7 +14,10 @@ books = books_block.select('li')
 books_data = []
 for book in books:
     image = book.find('div', attrs={"class": "image_container"}).find("img")['src']
-    print(image)
+    image_src_full = f"https://books.toscrape.com/{image}"
+    title = book.find('h3').find("a")['title']
+    price = book.find('div', attrs={"class": "product_price"}).find("p").text
+    print(f"{image_src_full:<90}{title:<100}{price}")
 
 
 
