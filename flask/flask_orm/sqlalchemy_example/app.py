59be6dac49718085463b38c0abba8f404f0afd17
VAS-IDE-Win-11-PC <vas.ide2022@gmail.com>
2023-10-31 15:01:17 +0300

    Tech flask orm fix

--- flask/flask_orm/sqlalchemy_example/app.py
+++ flask/flask_orm/sqlalchemy_example/app.py
@@ -3,13 +3,14 @@
 from flask import Flask, request, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
-from tech.flask.flask_innit.course10.flask_examples.with_templates import config
+from tech.flask.flask_orm.sqlalchemy_example import config
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(config)
 
 # http://flask-sqlalchemy.pocoo.org/2.1/quickstart/#a-minimal-application
 db = SQLAlchemy(app)
+app.app_context().push()
 
 
 @app.route('/', methods=['GET', 'POST'])
